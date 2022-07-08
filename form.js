f4c7aad70d46c5c844d646461b08e20b5aab799d
Валерия <sp.ether@yandex.ru>
2022-07-08 11:10:24 +0300

    add whole code

--- form.js
+++ form.js
@@ -1,2 +1,19 @@
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const data = new FormData(evt.target);
+  const formJSON = Object.fromEntries(data.entries());
+  const block = document.querySelector('div');
+  block.innerText = JSON.stringify(formJSON, null, 2)
+}
+
+const sendData = () => {
+  fetch('')
+    .then((response) => {
+      if (response.ok) {
+        alert(response.json());
+      }
+    })
+}
+
 const form = document.querySelector('form');
-console.log(form);
+form.addEventListener('submit', handleSubmit);
